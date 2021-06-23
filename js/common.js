437db417d7c50a0bf3ad4dfa8f7a24e0f38865c8
ITkah <ilatkacenko132@gmail.com>
2021-06-23 21:12:08 +0300

    ads

--- js/common.js
+++ js/common.js
@@ -1,34 +1,3 @@
-var swiper = new Swiper(".partners .swiper-container", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-        el: ".partners .swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        0: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-        },
-    },
-    navigation: {
-        nextEl: '.partners .swiper-button-next',
-        prevEl: '.partners .swiper-button-prev',
-    }
-});
-
 $(".header_lang_mob .header_lang>li>a").on("click", function(e) {
     e.preventDefault();
     $(this).siblings(".sub-menu").slideToggle(200);
